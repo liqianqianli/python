d6a83e353d9626794191bf4efdd4946cda9db044
LiQianQian <1159739262@qq.com>
2017-07-10 21:36:11 +0800

    上传代码

---
+++ shuzi.py
@@ -0,0 +1,16 @@
+﻿import random
+secret = random.randint(1,10)
+print(",,,,,,,,,,我爱鱼c工作室,,,,,,,,,")
+temp = input("猜数字：")
+guess = int(temp)
+while guess !=secret:
+    temp = input("猜错了请重新输入")
+    guess = int(temp)
+    if guess == secret:
+        print("你真是太聪明了")
+    else:
+        if guess > secret:
+            print("嘿嘿，大了")
+        else:
+            print("哎呦，小了")
+print("游戏结束，不玩啦")
